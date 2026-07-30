0e8c8b9efa77a39e331b4c158aebc266c0fb07e1
Varad8408 <deshpandevarad8408@gmail.com>
2026-07-30 06:56:17 +0530

    project Setup snd First problem

---
+++ src/basics/P001_PositiveOrNegativeNum.java
@@ -0,0 +1,16 @@
+package src.basics;
+
+import java.util.Scanner;
+
+public class P001_PositiveOrNegativeNum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int num = sc.nextInt();
+
+        if (num > 0) System.out.println(num + " is a positive Number");
+        else if (num < 0) System.out.println(num + " is a negative Number");
+        else System.out.println(num + " is a Zero");
+    }
+}
